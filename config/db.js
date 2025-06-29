4529ffa3755817e7baf818aeb75eb229a4b8ba2f
nhussein2026 <naser738nas@gmail.com>
2025-06-29 05:03:00 +0300

    feat: Set up server with routes and MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('Error connecting to MongoDB', error);
+    console.error("Error connecting to MongoDB", error);
     process.exit(1);
   }
 };
